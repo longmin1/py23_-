90753e1629e46bc0333738d0744ac7f0a8bed299
xixi_tech <xujuan@ceshiren.com>
2022-04-10 16:20:27 +0800

    实战练习1 参数化

---
+++ testing/conftest.py
@@ -0,0 +1,14 @@
+"""
+__author__ = '霍格沃兹测试开发学社'
+__desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/topic/15860'
+"""
+
+
+def pytest_collection_modifyitems(items):
+    """
+    测试用例收集完成时，将收集到的item的name和nodeid的中文显示
+    :return:
+    """
+    for item in items:
+        item.name = item.name.encode("utf-8").decode("unicode_escape")
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
--- testing/test_calc.py
+++ testing/test_calc.py
@@ -4,20 +4,79 @@ __desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/
 """
 
 # 测试模块
+import pytest
+
 from pythoncode.calculator import Calculator
 
 
 class TestCalculator:
+    def setup_class(self):
+        print("实例化calculator对象")
+        self.calc = Calculator()
+
     def setup(self):
-        pass
+        print("开始计算")
 
     def teardown(self):
-        pass
+        print("结束计算")
 
-    def test_add0(self):
+    def teardown_class(self):
+        print("结束测试")
+
+    # 冒烟测试用例
+    @pytest.mark.P0
+    # 需要至少两个参数，第一参数是字符串- 里面存放要传递的参数，以逗号隔开
+    # 第二参数，就是我们要传递的数据序列（可以列表，可以元组），每个序列里存放一组数据
+    @pytest.mark.parametrize('a,b, expect',
+                             [[1, 1, 2], [-0.01, 0.02, 0.01], [10, 0.02, 10.02]],
+                             ids=['整型', '浮点型', '整型+浮点型'])
+    def test_add0(self, a, b, expect):
         # 测试相加方法
-        calc = Calculator()
-        result = calc.add(1, 1)
+        result = self.calc.add(a, b)
         print(result)
         # 实际结果 对比 预期结果
-        assert result == 2
+        assert result == expect
+
+    # 有效边界值
+    @pytest.mark.P1
+    @pytest.mark.parametrize('a,b, expect',
+                             [[98.99, 99, 197.99],
+                              [99, 98.99, 197.99],
+                              [-98.99, -99, -197.99],
+                              [-99, -98.99, -197.99]],
+                             ids=['边界值-正1', '边界值-正2', '边界值-负1', '边界值-负2'])
+    def test_add1(self, a, b, expect):
+        result = self.calc.add(a, b)
+        assert result == expect
+
+    # 无效边界值
+    @pytest.mark.P1
+    @pytest.mark.parametrize('a,b, expect',
+                             [[99.01, 0, "参数大小超出范围"],
+                              [-99.01, -1, "参数大小超出范围"],
+                              [2, 99.01, "参数大小超出范围"],
+                              [1, -99.01, "参数大小超出范围"],
+                              ],
+                             ids=['边界值-参数超出范围1', '边界值-参数超出范围2',
+                                  '边界值-参数超出范围3', '边界值-参数超出范围4'])
+    def test_add2(self, a, b, expect):
+        result = self.calc.add(a, b)
+        assert result == expect
+
+    # 异常情况处理
+    @pytest.mark.P1
+    @pytest.mark.parametrize('a,b,errortype', [
+        ["文", 9.3, TypeError],
+        [4, "字", TypeError]
+    ])
+    def test_add3(self, a, b, errortype):
+        # pytest 封装的一种处理异常的方式
+        with pytest.raises(errortype) as e:
+            result = self.calc.add(a, b)
+
+        print(e.typename)
+        # try:
+        #     result = self.calc.add("中",9.3)
+        # except TypeError as e :
+        #     print("异常信息：")
+        #     print(e)
